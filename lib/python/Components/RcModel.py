5fe1d1b342a409c4d054a430f1461189015a0460
betacentauri <betacentauri@arcor.de>
2014-08-22 19:03:47 +0200

    Add et7x00 model

--- lib/python/Components/RcModel.py
+++ lib/python/Components/RcModel.py
@@ -22,8 +22,8 @@ class RcModel:
 	def getRcFolder(self):
 		remotefolder = 'dmm0'
 		rc = self.readFile('/proc/stb/ir/rc/type')
-                if os.path.exists('/etc/enigma2/EtRcType'):
-                        model = self.readFile('/etc/enigma2/EtRcType')
+		if os.path.exists('/etc/enigma2/EtRcType'):
+			model = self.readFile('/etc/enigma2/EtRcType')
 			if model == 'et6500':
 				remotefolder = 'et6500'
 			elif model == 'et9500':
@@ -36,27 +36,31 @@ class RcModel:
 				remotefolder = 'et4x00'
 			elif model == 'et6000':
 				remotefolder = 'et6x00'
+			elif model == 'et7000' or 'et7500':
+				remotefolder = 'et7x00'
 			return remotefolder
 		
-                else:
-		        if getBoxType() in ('et6500') and rc == '9':
-			        remotefolder = 'et6500'
-		        elif getBoxType() in ('et9500') and rc == '9':
-			        remotefolder = 'et9500'
-		        elif getBoxType() in ('et8000', 'et10000') and rc == '9':
-			        remotefolder = 'et8000'
-                        elif rc == '5':
-			        remotefolder = 'et9x00'
-                        elif rc == '7':
-			        remotefolder = 'et6x00'
-		        elif rc == '13':
-			        remotefolder = 'et4x00'
-		        elif rc == '11':
-			        remotefolder = 'et9x00'
+		else:
+			if getBoxType() in ('et6500') and rc == '9':
+				remotefolder = 'et6500'
+			elif getBoxType() in ('et9500') and rc == '9':
+				remotefolder = 'et9500'
+			elif getBoxType() in ('et8000', 'et10000') and rc == '9':
+				remotefolder = 'et8000'
+			elif getBoxType() in ('et7000', 'et7500'):
+				remotefolder = 'et7x00'
+			elif rc == '5':
+				remotefolder = 'et9x00'
+			elif rc == '7':
+				remotefolder = 'et6x00'
+			elif rc == '13':
+				remotefolder = 'et4x00'
+			elif rc == '11':
+				remotefolder = 'et9x00'
 
 		return remotefolder
-                
-        def getRcLocation(self):
+
+	def getRcLocation(self):
 		baselocation = '/usr/share/enigma2/rc_models/'
 		remotefolder = self.getRcFolder()
 		return baselocation + remotefolder + '/'
