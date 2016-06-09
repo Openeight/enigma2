ccacd5260abfbbc59badb73a3b092450550186c7
mogli123 <onevibes@gmx.net>
2016-06-09 18:11:47 +0200

    [Common Interface] added Ci Autopin (part 1 of 2)

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -595,6 +595,8 @@ def InitUsageConfig():
 	if SystemInfo["GraphMultiEpg"]:
 		config.misc.graph_mepg = ConfigSubsection()
 		config.misc.graph_mepg.extension_menu = ConfigYesNo(default = True)
+		
+	config.misc.ci_autopin = ConfigYesNo(default = False)
 
 def updateChoices(sel, choices):
 	if choices:
