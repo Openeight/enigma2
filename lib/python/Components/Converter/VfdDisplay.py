ab7ccfc99ba7f266e2ec19250064d30ae7474873
xtrendmaster <amicimax@aol.com>
2018-10-21 14:16:09 +0100

    Add 7segment support

--- lib/python/Components/Converter/VfdDisplay.py
+++ lib/python/Components/Converter/VfdDisplay.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 
+from enigma import iPlayableService
 from Poll import Poll
 from Components.Converter.Converter import Converter
 from Components.Element import cached
@@ -30,9 +31,9 @@ class VfdDisplay(Poll, Converter, object):
 				if 'loop' in self.type and self.delay:
 					self.loop = self.delay
 			if 'nozero' in self.type:
-				self.hour = '%-'
-			else:
 				self.hour = '%'
+			else:
+				self.hour = '%02'
 			if '12h' in self.type:
 				self.hour = self.hour + 'I'
 			else:
@@ -53,13 +54,13 @@ class VfdDisplay(Poll, Converter, object):
 				else:
 					self.poll_interval = 1000
 					self.showclock = 3
-				clockformat = self.hour + '%M'
+				clockformat = self.hour + '%02M'
 			elif self.showclock == 2:
 				self.showclock = 3
-				clockformat = self.hour + '%M'
+				clockformat = self.hour + '%02M'
 			else:
 				self.showclock = 2
-				clockformat = self.hour + ':%M'
+				clockformat = self.hour + ':%02M'
 			if self.loop != -1:
 				self.loop -= 1000
 				if self.loop <= 0:
@@ -70,7 +71,7 @@ class VfdDisplay(Poll, Converter, object):
 	text = property(getText)
 
 	def changed(self, what):
-		if what[0] is self.CHANGED_SPECIFIC and self.delay >= 0:
+		if what[0] is self.CHANGED_SPECIFIC and self.delay >= 0 and what[1] == iPlayableService.evStart:
 			self.showclock = 0
 			if self.loop != -1:
 				self.loop = self.delay
@@ -79,3 +80,4 @@ class VfdDisplay(Poll, Converter, object):
 			Converter.changed(self, what)
 		elif what[0] is self.CHANGED_POLL:
 			Converter.changed(self, what)
+
