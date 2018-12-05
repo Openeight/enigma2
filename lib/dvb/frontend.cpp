bdfd8a62c3c15eff8ed667df8ac23753893f1239
Betacentauri <betacentauri@arcor.de>
2018-12-05 17:35:44 +0100

    Fix snr max db for tuner Si2166D

--- lib/dvb/frontend.cpp
+++ lib/dvb/frontend.cpp
@@ -1177,7 +1177,12 @@ void eDVBFrontend::calculateSignalQuality(int snr, int &signalquality, int &sign
 				break;
 		}
 	}
-    else if (!strncmp(m_description, "Si216", 5)) // all new Models with SI Tuners
+	else if (!strncmp(m_description, "Si2166D", 7)) // SF8008 S2
+	{
+		ret = snr;
+		sat_max = 1620;
+	}
+	else if (!strncmp(m_description, "Si216", 5)) // all new Models with SI Tuners
 	{
 		ret = snr;
 	}
