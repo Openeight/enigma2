436b5c4cf943a8a648735893cd258a35bf617cc1
xtrendmaster <amicimax@aol.com>
2019-01-21 14:12:02 +0000

    [Radiologo] try fix 1

--- lib/dvb/decoder.cpp
+++ lib/dvb/decoder.cpp
@@ -1210,6 +1210,7 @@ RESULT eTSMPEGDecoder::showSinglePic(const char *filename)
 			if (m_video_clip_fd >= 0)
 			{
 #if HAVE_HISILICON
+			if (m_video_clip_fd >= 0)
 				finishShowSinglePic();
 #endif
 				bool seq_end_avail = false;
@@ -1251,8 +1252,11 @@ RESULT eTSMPEGDecoder::showSinglePic(const char *filename)
 				if (!seq_end_avail)
 					write(m_video_clip_fd, seq_end, sizeof(seq_end));
 				writeAll(m_video_clip_fd, stuffing, 8192);
-#if !HAVE_HISILICON
+#if HAVE_HISILICON
+				;
+#else
 				m_showSinglePicTimer->start(150, true);
+#endif
 #endif
 			}
 			close(f);
