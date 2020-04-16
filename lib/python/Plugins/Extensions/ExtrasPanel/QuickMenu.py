eabe3016938a96bdde5835fa0017399daaa4186a
massimo <amicimax@aol.com>
2020-04-16 16:15:28 +0100

    re-commit last changes ... i have deleted by mistake

--- lib/python/Plugins/Extensions/ExtrasPanel/QuickMenu.py
+++ lib/python/Plugins/Extensions/ExtrasPanel/QuickMenu.py
@@ -23,6 +23,7 @@ from Screens.SkinSelector import SkinSelector, LcdSkinSelector
 from Screens.RecordPaths import RecordPathsSettings
 from Screens.CCcamInfo import CCcamInfoMain
 from Screens.OScamInfo import OscamInfoMenu
+from Screens.FlashImage import SelectImage
 from Screens.Hotkey import HotkeySetup
 from Plugins.SystemPlugins.Videomode.plugin import videoSetupMain
 from Plugins.Plugin import PluginDescriptor
@@ -39,7 +40,6 @@ from Plugins.Extensions.ExtrasPanel.plugin import ShowSoftcamPanelExtensions
 from Plugins.Extensions.ExtrasPanel.SoftwarePanel import SoftwarePanel
 from Plugins.Extensions.ExtrasPanel.sundtek import SundtekControlCenter
 from Addons import AddonsFileBrowser
-from Plugins.SystemPlugins.SoftwareManager.Flash_online import FlashOnline
 from Plugins.SystemPlugins.SoftwareManager.ImageBackup import ImageBackup
 from Plugins.SystemPlugins.SoftwareManager.plugin import UpdatePlugin, SoftwareManagerSetup
 from Plugins.SystemPlugins.SoftwareManager.BackupRestore import BackupScreen, RestoreScreen, BackupSelection, getBackupPath, getOldBackupPath, getBackupFilename
@@ -562,7 +562,7 @@ class QuickMenu(Screen):
 			self.SatfinderMain()
 		elif item[0] == _('Software Update'):
 			self.session.open(SoftwarePanel)
-		elif item[0] == _('Flash Online'):
+		elif item[0] == _('SelectImage'):
 			self.session.open(FlashOnline)
 		elif item[0] == _('Complete Backup'):
 			if DFLASH == True:
