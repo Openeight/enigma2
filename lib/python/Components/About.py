8349096e7318f77f06000b01cd4c54d0fe2affe7
Betacentauri <betacentauri@arcor.de>
2020-04-28 19:26:54 +0200

    Show proper last update date

--- lib/python/Components/About.py
+++ lib/python/Components/About.py
@@ -35,7 +35,7 @@ def getBuildDateString():
 def getUpdateDateString():
 	try:
 		from glob import glob
-		build = [x.split("-")[-2:-1][0][-8:] for x in open(glob("/var/lib/opkg/info/openpli-bootlogo.control")[0], "r") if x.startswith("Version:")][0]
+		build = [x.split("-")[-2:-1][0][-8:] for x in open(glob("/var/lib/opkg/info/oe-alliance-branding.control")[0], "r") if x.startswith("Version:")][0]
 		if build.isdigit():
 			return  "%s-%s-%s" % (build[:4], build[4:6], build[6:])
 	except:
