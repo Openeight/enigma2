37ba101b2d87fd7424a710d1191ab0fb21d4ebe1
Smokey <atom89@users.noreply.github.com>
2020-11-29 18:39:48 +0100

    Update ScriptRunner.py
    Activate extra buttons for extra functions 'Run hidden' and 'Run and Close on success'.

--- lib/python/Plugins/Extensions/ExtrasPanel/ScriptRunner.py
+++ lib/python/Plugins/Extensions/ExtrasPanel/ScriptRunner.py
@@ -20,8 +20,12 @@ class ScriptRunner(Screen):
 	skin = """<screen name="ScriptRunner" position="center,center" size="560,400" title="Script Runner" flags="wfBorder" >
 		<ePixmap pixmap="skin_default/buttons/red.png" position="0,0" size="140,40" alphatest="on" />
 		<ePixmap pixmap="skin_default/buttons/green.png" position="140,0" size="140,40" alphatest="on" />
+		<ePixmap pixmap="skin_default/buttons/yellow.png" position="280,0" size="140,40" alphatest="on" />
+		<ePixmap pixmap="skin_default/buttons/blue.png" position="420,0" size="140,40" alphatest="on" />
 		<widget name="key_red" position="0,0" zPosition="1" size="140,40" font="Regular;20" halign="center" valign="center" backgroundColor="#9f1313" transparent="1" />
 		<widget name="key_green" position="140,0" zPosition="1" size="140,40" font="Regular;20" halign="center" valign="center" backgroundColor="#1f771f" transparent="1" />
+		<widget name="key_yellow" position="280,0" zPosition="1" size="140,40" font="Regular;20" halign="center" valign="center" backgroundColor="#a08500" transparent="1" />
+		<widget name="key_blue" position="420,0" zPosition="1" size="140,40" font="Regular;20" halign="center" valign="center" backgroundColor="#18188b" transparent="1" />
 		<widget name="lab1" position="0,50" size="560,50" font="Regular; 20" zPosition="2" transparent="0" halign="center"/>
 		<widget name="list" position="10,105" size="540,300" scrollbarMode="showOnDemand" />
 		<applet type="onLayoutFinish">
@@ -35,18 +39,23 @@ class ScriptRunner(Screen):
 		Screen.setTitle(self, _("Job Manager"))
 		self['lab1'] = Label()
 		self.list = []
+		self.execute = ""
 		self.populate_List()
 		self['list'] = MenuList(self.list)
 		self['myactions'] = ActionMap(['ColorActions', 'OkCancelActions', 'DirectionActions'],
 			{
 				'cancel': self.close,
 				'red': self.close,
-				'green': self.runscript,
-				'ok': self.runscript,
+				'green': self.green,
+				'yellow': self.yellow,
+				'blue': self.blue,
+				'ok': self.green,
 			}, -1)
 
 		self["key_red"] = Button(_("Close"))
 		self["key_green"] = Button(_("Run"))
+		self["key_yellow"] = Button(_("Run hidden"))
+		self["key_blue"] = Button(_("Run and Close on success"))
 		
 	def populate_List(self):
 		if not path.exists('/usr/script'):
@@ -60,6 +69,18 @@ class ScriptRunner(Screen):
 			if pkg.find('.sh') >= 0:
 				self.list.append(pkg)
 		self.list.sort()	
+
+	def green(self):
+		self.execute = "0"
+		self.runscript()
+
+	def yellow(self):
+		self.execute = "1"
+		self.runscript()
+
+	def blue(self):
+		self.execute = "2"
+		self.runscript()
 
 	def runscript(self):
 		self.sel = self['list'].getCurrent()
@@ -76,10 +97,16 @@ class ScriptRunner(Screen):
 				chmod("/usr/script/" + self.sel, 0755)
 			cmd1 = ". /usr/script/" + self.sel
 			if ".hidden." in self.sel:
+				self.execute = "1"
+			elif ".close." in self.sel:
+				self.execute = "2"
+			if self.execute == "1":
 				from enigma import eConsoleAppContainer
 				eConsoleAppContainer().execute(cmd1)
+			elif self.execute == "2":
+				self.session.open(Console, title=self.sel, cmdlist = [cmd1], closeOnSuccess = True)
 			else:
-				self.session.open(Console, title=self.sel, cmdlist = [cmd1], closeOnSuccess = False)	
+				self.session.open(Console, title=self.sel, cmdlist = [cmd1])	
 					
 	def myclose(self):
 		self.close()
