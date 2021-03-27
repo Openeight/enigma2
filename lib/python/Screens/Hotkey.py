c315404a8e1dd3b1caf3131de1cf2dce6ca09562
Betacentauri <betacentauri@arcor.de>
2021-03-27 12:30:15 +0100

    Add f4 key

--- lib/python/Screens/Hotkey.py
+++ lib/python/Screens/Hotkey.py
@@ -28,6 +28,8 @@ class hotkey:
 		("F2" + " " + _("long"), "f2_long", ""),
 		("F3", "f3", ""),
 		("F3" + " " + _("long"), "f3_long", ""),
+		("F4", "f4", ""),
+		("F4" + " " + _("long"), "f4_long", ""),
 		(_("Red"), "red", ""),
 		(_("Green"), "green", "Module/Screens.PluginBrowser/PluginBrowser"),
 		(_("Yellow"), "yellow", ""),
