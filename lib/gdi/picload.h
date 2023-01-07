298ef8ff1a074303622f3e285aed9f5a635a655d
Betacentauri <betacentauri@arcor.de>
2023-01-07 13:13:33 +0100

    PicLoad: Set auto_orientation default to false
    to be compatible with other images which don't have the
    parameter

--- lib/gdi/picload.h
+++ lib/gdi/picload.h
@@ -104,7 +104,7 @@ public:
 	RESULT startDecode(const char *filename, int x=0, int y=0, bool async=true);
 	RESULT getThumbnail(const char *filename, int x=0, int y=0, bool async=true);
 	RESULT setPara(PyObject *val);
-	RESULT setPara(int width, int height, double aspectRatio, int as, bool useCache, int resizeType, const char *bg_str, bool auto_orientation);
+	RESULT setPara(int width, int height, double aspectRatio, int as, bool useCache, int resizeType, const char *bg_str, bool auto_orientation = false);
 	PyObject *getInfo(const char *filename);
 	SWIG_VOID(int) getData(ePtr<gPixmap> &SWIG_OUTPUT);
 };
